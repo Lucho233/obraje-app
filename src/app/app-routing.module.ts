e0b2ab369e2d6c9ee70541d3710835615a16e95a
benja-MM <benjamarmol24@gmail.com>
2019-11-01 11:12:45 -0300

    Agrego servicio de noticias y actualizo inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: InicioComponent, pathMatch: "full"},
+  // {path: "/contenido", component: VerContenidoComponent},
+  // {path: "/chat", component: ChatComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NoticiasService } from '../services/noticias.service';
 
 @Component({
   selector: 'app-inicio',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.component.scss']
 })
 export class InicioComponent implements OnInit {
-
-  constructor() { }
+  listaNoticias = []
+  constructor(private noticiasService: NoticiasService) { }
 
   ngOnInit() {
+    this.noticiasService.getAllNotices().subscribe((noticias) => {
+      this.listaNoticias = noticias
+    })
   }
 
 }
